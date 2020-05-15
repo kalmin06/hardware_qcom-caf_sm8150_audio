750934ad67a630df1cdeb2983345d721afea2937
kalmin06 <suneeshh4k@gmail.com>
2020-05-15 18:35:21 +0000

    Bringup Nitrogen

--- hal/audio_extn/Android.mk
+++ hal/audio_extn/Android.mk
@@ -36,7 +36,7 @@ LOCAL_C_INCLUDES := \
     system/media/audio_utils/include \
     external/expat/lib \
     $(call include-path-for, audio-route) \
-    $(TOP)/hardware/qcom/audio-caf/sm8150/hal \
+    $(TOP)/hardware/qcom/audio-caf-sm8150 \
     $(call include-path-for, audio-effects)
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -94,8 +94,8 @@ LOCAL_C_INCLUDES := \
     system/media/audio_utils/include \
     external/expat/lib \
     $(call include-path-for, audio-route) \
-    $(TOP)/hardware/qcom/audio-caf/sm8150/hal \
-    $(TOP)/hardware/qcom/audio-caf/sm8150/hal/$(AUDIO_PLATFORM) \
+    $(TOP)/hardware/qcom/audio-caf-sm8150/ \
+    $(TOP)/hardware/qcom/audio-caf-sm8150/$(AUDIO_PLATFORM) \
     $(call include-path-for, audio-effects)
 
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -119,7 +119,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libssrec
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -181,7 +181,7 @@ LOCAL_MODULE := libhdmiedid
 LOCAL_MODULE_OWNER := third_party
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -231,12 +231,12 @@ LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_HEADER_LIBRARIES += libsystem_headers
 include $(BUILD_SHARED_LIBRARY)
 
-#--------------------------------------------
+#-------------------------------------------
 #          Build SPKR_PROTECT LIB
 #--------------------------------------------
 include $(CLEAR_VARS)
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
   # B-family platform uses msm8974 code base
   AUDIO_PLATFORM := msm8974
@@ -292,7 +292,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
   # B-family platform uses msm8974 code base
   AUDIO_PLATFORM := msm8974
@@ -351,7 +351,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := liba2dpoffload
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -411,7 +411,7 @@ LOCAL_MODULE := libexthwplugin
 
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -470,7 +470,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libhfp
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -531,7 +531,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libhdmipassthru
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -595,7 +595,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libbatterylistener
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -665,7 +665,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libhwdepcal
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona lito atoll sdm660 msm8937 msm8998 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -724,7 +724,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE:= libmaxxaudio
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona sdm660 msm8937 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
@@ -781,7 +781,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE:= libaudiozoom
 LOCAL_VENDOR_MODULE := true
 
-PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf/sm8150/hal
+PRIMARY_HAL_PATH := $(TOP)/hardware/qcom/audio-caf-sm8150
 AUDIO_PLATFORM := $(TARGET_BOARD_PLATFORM)
 
 ifneq ($(filter sdm845 sdm710 sdmshrike msmnile kona sdm660 msm8937 $(MSMSTEPPE) $(TRINKET),$(TARGET_BOARD_PLATFORM)),)
